54aaa51ffde2b1a5f80a91cb54d7309b25109be7
gustavo <banuelos.201021581@hotmail.com>
2019-07-09 17:47:40 -0600

    Preparando app para publicar
    ready

--- TakeControlProgress/Assets/Resources/Scripts/Model/Session.cs
+++ TakeControlProgress/Assets/Resources/Scripts/Model/Session.cs
@@ -3,4 +3,5 @@
     public static Firebase.Auth.FirebaseAuth auth;
     public static Firebase.Auth.Credential credential;
     public static User currentUser;
+    public static bool[] stars= new bool [5];
 }
